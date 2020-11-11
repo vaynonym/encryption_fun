edaccf0a5608474fdb7ad246ef54692125c15cbf
Vaynonym <timruschke@googlemail.com>
2020-11-11 20:04:04 +0100

    removed methods that are already moved somewhere else

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -28,10 +28,3 @@ def diffie_hellman_syn_ack(gb: int, n: int, a: int) -> int:
     return result
 
 
-def caesar_cipher_encrypt(text: str, shift_amount: int):
-    return "".join([chr((ord(char) + shift_amount) % 256) if char != '\n' else "\n" for char in text])
-
-
-def caesar_cipher_decrypt(text: str, shift_amount: int):
-    return "".join([chr((ord(char) + 256 - shift_amount) % 256) if char != '\n' else "\n" for char in text])
-
